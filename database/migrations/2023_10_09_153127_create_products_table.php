7866085512af0d34994f7a29456fc9640a0261dd
phong.nguyen@monkey.edu.vn <pn463187@gmail.com>
2023-10-09 23:29:04 +0700

    created database

--- database/migrations/2023_10_09_153127_create_products_table.php
+++ database/migrations/2023_10_09_153127_create_products_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->integer('level');
+            $table->string('description');
+            $table->double('price');
+            $table->integer('qty');
+            $table->integer('discount')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
